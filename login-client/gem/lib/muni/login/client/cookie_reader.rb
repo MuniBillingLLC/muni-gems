ecb82fcce52d22eff31e2e39576449cb086f971d
Drago Todorov <todomodo@nospam.local>
2023-12-15 11:08:05 -0800

    version 0.0.12 * sid_cookie_name now configurable

--- login-client/gem/lib/muni/login/client/cookie_reader.rb
+++ login-client/gem/lib/muni/login/client/cookie_reader.rb
@@ -2,18 +2,21 @@ module Muni
   module Login
     module Client
       class CookieReader
+        include ::ActiveSupport::Configurable
 
-        SID_TOKEN = "sid_token".freeze
+        config_accessor :config_sid_cookie_name
 
         def initialize(plain_cookies:)
           @plain_cookies = plain_cookies
         end
 
         def sid_token
-          @plain_cookies[SID_TOKEN]
+          @plain_cookies[sid_cookie_name]
         end
 
-        private
+        def sid_cookie_name
+          self.config_sid_cookie_name || ENV['MUNI_SID_COOKIE_NAME']
+        end
 
       end
     end
--- login-client/gem/spec/lib/cookie_reader_spec.rb
+++ login-client/gem/spec/lib/cookie_reader_spec.rb
@@ -7,14 +7,16 @@ RSpec.describe Muni::Login::Client::CookieReader do
   end
 
   describe "#sid_token" do
-    let(:value) { random_hex_string }
+    let(:cookie_value) { random_hex_string }
+    let(:cookie_name) { random_hex_string }
     let(:plain_cookies) do
       {
-        described_class::SID_TOKEN => value
+        cookie_name => cookie_value
       }
     end
     it do
-      expect(subj.sid_token).to eq(value)
+      allow(subj).to receive(:sid_cookie_name).and_return(cookie_name)
+      expect(subj.sid_token).to eq(cookie_value)
     end
   end
 
