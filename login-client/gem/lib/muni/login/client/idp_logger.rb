72eb81896394b7fc8abbfe27e8a8605e522b5f2b
Drago Todorov <drago@munibilling.com>
2024-02-18 12:23:25 -0800

    improve logging

--- login-client/gem/lib/muni/login/client/idp_logger.rb
+++ login-client/gem/lib/muni/login/client/idp_logger.rb
@@ -3,6 +3,7 @@ module Muni
   module Login
     module Client
       class IdpLogger
+        MUNI_GEM_VERSION = "0.0.18" # keep in sync with "muni-login-client.gemspec"
 
         def initialize
           @rails_logger = Rails.logger
@@ -13,35 +14,43 @@ module Muni
         end
 
         def debug(message)
-          @rails_logger.debug decorate(message)
+          @rails_logger.debug decorate(message: message, level: "debug")
         end
 
         def info(message)
-          @rails_logger.info decorate(message)
+          @rails_logger.info decorate(message: message, level: "info")
         end
 
         def warn(message)
-          @rails_logger.warn decorate(message)
+          @rails_logger.warn decorate(message: message, level: "warn")
         end
 
         def error(message)
-          @rails_logger.error decorate(message)
+          @rails_logger.error decorate(message: message, level: "error")
         end
 
         # use this method to dump all kinds of information for troubleshooting
         # never enable this in prod, since traces may contain privileged
         # information
         def trace(message)
-          info("***TRACE: #{message}") if ENV['MUNIDEV_IDPLOG_TRACE'].present?
+          if ENV['MUNIDEV_IDPLOG_TRACE'].present?
+            @rails_logger.info decorate(message: message, level: "trace")
+          end
         end
 
         private
 
-        def decorate(message)
-          return if message.nil?
-          return message unless @idrequest.present?
-          return message unless message.is_a?(Hash)
-          message.merge(action: @idrequest.action_signature)
+        def decorate(message:, level:)
+          msg_hash = {
+            level: level,
+            gem_version: MUNI_GEM_VERSION,
+            message: message
+          }
+          if @idrequest.present?
+            msg_hash[:action] = @idrequest.action_signature
+          end
+
+          msg_hash
         end
 
       end
