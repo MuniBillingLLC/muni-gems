aaeaa78eab0b8f9512901ff6e6f01e03b76d633e
Drago Todorov <drago@munibilling.com>
2025-02-25 11:30:22 -0800

    version 0.0.40: method for deleting sid tokens

--- login-client/gem/lib/muni/login/client/cookie_reader.rb
+++ login-client/gem/lib/muni/login/client/cookie_reader.rb
@@ -3,17 +3,25 @@ module Muni
     module Client
       class CookieReader
 
-        def initialize(plain_cookies:)
+        def initialize(plain_cookies:, top_level_domain: nil)
           @plain_cookies = plain_cookies
+          @top_level_domain = top_level_domain
         end
 
         def sid_token
           plain_cookies[sid_cookie_name]
         end
 
+        # this is somewhat hacky, there should be no deletion method on a pure
+        # reader. However, it simplifies some downstream operations, so here we
+        # have
+        def delete_sid_token
+          plain_cookies.delete(sid_cookie_name, domain: top_level_domain)
+        end
+
         private
 
-        attr_reader :plain_cookies
+        attr_reader :plain_cookies, :top_level_domain
 
         delegate :sid_cookie_name, to: :gem_settings
 
--- login-client/gem/spec/lib/cookie_reader_spec.rb
+++ login-client/gem/spec/lib/cookie_reader_spec.rb
@@ -1,22 +1,39 @@
 require 'spec_helper'
 
 RSpec.describe Muni::Login::Client::CookieReader do
+  let(:cookie_name) { random_hex_string }
+  let(:cookie_value) { random_hex_string }
+  let(:top_level_domain) { random_hex_string }
 
   let(:subj) do
-    described_class.new(plain_cookies: plain_cookies)
+    described_class.new(
+      plain_cookies: plain_cookies,
+      top_level_domain: top_level_domain)
+  end
+
+  before do
+    allow(subj).to receive(:sid_cookie_name).and_return(cookie_name)
   end
 
   describe "#sid_token" do
-    let(:cookie_value) { random_hex_string }
-    let(:cookie_name) { random_hex_string }
     let(:plain_cookies) do
       {
         cookie_name => cookie_value
       }
     end
     it do
-      allow(subj).to receive(:sid_cookie_name).and_return(cookie_name)
       expect(subj.sid_token).to eq(cookie_value)
+    end
+  end
+
+  describe "#delete_sid_token" do
+    let(:plain_cookies) { instance_double(ActionDispatch::Cookies::CookieJar, delete: nil) }
+    it do
+      expect(plain_cookies)
+        .to receive(:delete)
+              .with(cookie_name, domain: top_level_domain)
+
+      subj.delete_sid_token
     end
   end
 
